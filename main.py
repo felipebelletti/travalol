7bf6dd082ea625d917d98f61970cbb596de71538
Fex0r <40951999+fex0r@users.noreply.github.com>
2021-01-27 19:26:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 tirando isso, nao acho que a intencao fosse somente de "ajudar", se fosse, voces nao estariam lucrando com o projeto ( alem de monopolizar na mao dos poucos que podem pagar, enfimmm :^) )
 o '''exploit''' (bota aspas nisso) no modelo atual so afeta filas ranqueadas, se quiser mudar isso, altere o $lobbycreatedata pro json respectivo a fila que voce quer (dps eu mexo nisso e torno  global)
 pelo que notei, quanto mais forte o hardware, mais fodido sera pra quem for travado. isso porque sera uma taxa maior de invites/kicks ocorrendo, assim incidindo numa maior chance das duas filas acontecerem ao mesmo tempo e a vulnerabilidade triggar
+alem disso, quanto mais pessoas rodando sobre o mesmo alvo = mais eficiente sera etc etc
 """
 
 def getsummonerid(summonnername):
